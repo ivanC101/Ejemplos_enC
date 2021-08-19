d17876c6a21549c67e2f59f8801d5aa50cdd5ef1
ivanCordova777 <al2143069409@azc.uam.mx>
2021-08-19 17:10:40 -0500

    Programa - Hola mundo
    Se hacen los ajustes para poder compilar y ejecutar en VS.

---
+++ main.c
@@ -0,0 +1,22 @@
+/*Comentarios para mi yo del futuro
+nombre del programa
+Autor
+Modificado por
+Que hace el programa
+necesita un circuito
+colocar una imagen en el repositorio (liga)
+terminales que se utilizan. el modo
+ejemplo: gpio 5_salida*/
+
+/*Bibliotecas*/
+#include <stdio.h>
+
+/*declaracion de variables globales*/
+
+/*funcion main*/
+
+int main()
+{
+    printf("Buen dia\n");
+    return 0;
+}
